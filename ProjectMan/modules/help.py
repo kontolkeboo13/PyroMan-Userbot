002c7bd574f9ea2187cf97e240ebef6f4f6f7d62
kontolkeboo13 <130371222+kontolkeboo13@users.noreply.github.com>
2023-04-11 18:04:21 +0700

    Update help.py

--- ProjectMan/modules/help.py
+++ ProjectMan/modules/help.py
@@ -28,24 +28,24 @@ async def module_help(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "PyroMan-UserBot Modules"
+        ac.title = "UserBot Modules"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @Lunatic0de × @SharingUserbot •"
+            message, f"```{str(ac)}```\n• @xxgramz •"
         )
         await message.reply(
-            f"**Contoh Ketik** `{CMD_HANDLER}help afk` **Untuk Melihat Informasi Module**"
+            f"**Contoh Ketik** `{CMD_HANDLER}help Afk` **Untuk Melihat Informasi Module**"
         )
 
     if help_arg:
         if help_arg in CMD_HELP:
             commands: dict = CMD_HELP[help_arg]
-            this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
+            this_command = f"──「 **Help For✨{str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `{CMD_HANDLER}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Lunatic0de"
+            this_command += "™ @xxgramz"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
